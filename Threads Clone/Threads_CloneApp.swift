f9927f0eab422e064e236f4d706464c68a66911e
Hassan Amjad <hassanamjad9529@gmail.com>
2025-03-07 22:40:22 +0500

    Initial Commit

---
+++ Threads Clone/Threads_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneApp.swift
+//  Threads Clone
+//
+//  Created by Hassan Amjad on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Threads_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Threads CloneTests/Threads_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneTests.swift
+//  Threads CloneTests
+//
+//  Created by Hassan Amjad on 07/03/2025.
+//
+
+import Testing
+@testable import Threads_Clone
+
+struct Threads_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
